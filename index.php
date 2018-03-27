086d0e995e12690352059d0b5c01a85accf595f5
vanessa <vanessa.chaillou@orange.fr>
2018-03-27 11:22:03 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>TODOLIST</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700|Pacifico" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<header class="site-header">
+		<h1>Liste de tâches</h1>
+	</header>
+
+	<main class="site-main">
+		<!--Categories-->
+		<div class="categories-list">
+			<h2 class="bloc-title">Catégories</h2>
+		</div>
+		<!--Tasks-->
+		<div class="tasks-list">
+			<h2 class="bloc-title">Tâches</h2>
+		</div>
+
+	</main>
+
+	<footer class="site-footer">
+		<p>Réalisé par l'équipe de <a href="http://simplon.co">Simplon Rodez</a></p>
+		
+	</footer>
+</body>
+</html>
