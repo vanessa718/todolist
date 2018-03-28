2263bb4c202343850983e102a5bb562ac2d58347
vanessa <vanessa.chaillou@orange.fr>
2018-03-28 14:54:57 +0200

    liste catégorie- ajout catégorie- menu déroulant + CSS

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>TODOLIST</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description">
+	<title>TODOLIST</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700|Pacifico" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
@@ -18,6 +17,35 @@
 		<!--Categories-->
 		<div class="categories-list">
 			<h2 class="bloc-title">Catégories</h2>
+			<ul class="cat-list">
+				<li>
+					<a href="/category-1" class="category color-red cat-default">Catégorie 1</a>
+				</li>
+				<li>
+					<a href="/category-2" class="category color-blue">Catégorie 2</a>
+				</li>
+				<li>
+					<a href="/category-3" class="category color-orange">Catégorie 3</a>
+				</li>
+				<li>
+					<a href="/category-4" class="category color-pink">Catégorie 4</a>
+				</li>
+			</ul>
+			<button class="add-item">Ajouter une catégorie</button>
+			<form class="form" method="POST">
+				<label class="form-label" for="cat-name">Nom de la catégorie<abbr title="champ obligatoire">*</abbr></label>
+				<input class="form-field" id="cat-name" name="cat-name" type="text" required>
+				<label class="form-label" for="cat-color">Couleur de la catégorie<abbr title="champ obligatoire">*</abbr></label>
+				<select class="form-field" id="cat-color" name="cat-color" required>
+					<option value="red">Rouge</option>
+					<option value="blue">Bleu</option>
+					<option value="orange">Orange</option>
+					<option value="pink">Rose</option>
+					<option value="green">Vert</option>
+					<option value="grey">Gris</option>
+				</select>
+				<button class="form-btn" type="submit">Ajouter</button>
+			</form>
 		</div>
 		<!--Tasks-->
 		<div class="tasks-list">
